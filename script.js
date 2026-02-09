f53950defcbf904504d16f5b91044012a70a5806
agarwalsvivek <agarwal.s.vivek@gmail.com>
2026-02-09 07:57:39 -0500

    adding initial html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function getAllLabels() {
+    return document.getElementsByTagName("label");
+}
+
+window.onload = function () {
+    // query all html label tags
+    const labels = document.getElementsByTagName("label");
+}
